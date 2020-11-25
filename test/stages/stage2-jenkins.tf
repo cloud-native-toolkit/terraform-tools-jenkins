29482752e5233f47d3792e0b78cd46a2d152c158
Sean Sundberg <seansund@us.ibm.com>
2020-11-25 00:38:26 -0600

    Adds workflows and config for module catalog

--- test/stages/stage2-jenkins.tf
+++ test/stages/stage2-jenkins.tf
@@ -1,9 +1,9 @@
 module "dev_tools_jenkins" {
   source = "./module"
 
+  cluster_type             = module.dev_cluster.type_code
   cluster_ingress_hostname = module.dev_cluster.ingress_hostname
   cluster_config_file      = module.dev_cluster.config_file_path
-  cluster_type             = module.dev_cluster.type_code
   tls_secret_name          = module.dev_cluster.tls_secret_name
   server_url               = module.dev_cluster.server_url
   tools_namespace          = module.dev_capture_state.namespace
