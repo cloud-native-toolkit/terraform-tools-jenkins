c3efd54f25e51811023400af42f577d13f05238d
Sean Sundberg <seansund@us.ibm.com>
2021-06-29 22:47:23 -0500

    Adds validations for deploy on iks (#6)
    * Adds iks_latest to test matrix
    * Adds *.iml to gitignore
    * Updates test variables
    * Changes cli-tools to v13
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1b-capture-state.tf
+++ test/stages/stage1b-capture-state.tf
@@ -1,8 +1,8 @@
 module "dev_capture_state" {
   source = "github.com/ibm-garage-cloud/terraform-k8s-capture-state"
 
-  cluster_type             = module.dev_cluster.type_code
-  cluster_config_file_path = module.dev_cluster.config_file_path
+  cluster_type             = module.dev_cluster.platform.type_code
+  cluster_config_file_path = module.dev_cluster.platform.kubeconfig
   namespace                = module.dev_tools_namespace.name
   output_path              = "${path.cwd}/cluster-state/before"
 }
